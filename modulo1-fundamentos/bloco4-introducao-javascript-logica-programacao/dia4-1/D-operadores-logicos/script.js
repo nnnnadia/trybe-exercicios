c8d8cda484f9a68080ed90385eaf2aca0816162a
Nádia <tristaonadia@gmail.com>
2022-02-02 15:48:02 -0300

    exercicios do dia 4-1: operadores lógicos.

---
+++ modulo1-fundamentos/bloco4-introducao-javascript-logica-programacao/dia4-1/D-operadores-logicos/script.js
@@ -0,0 +1,26 @@
+const currentHour = 15;
+let message = "";
+
+if (currentHour >= 22) {
+  message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+  message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+  message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11 && currentHour < 14) {
+  message = "Hora do almoço!!!";
+} else if (currentHour >= 4 && currentHour < 11) {
+  message = "Hmmm, cheiro de café recém passado";
+}
+
+console.log(message);
+
+let weekDay = "segunda-feira";
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else if (weekDay == "sábado" || weekDay == "domingo") {
+  console.log("FINALMENTE, descanso merecido UwU");
+} else {
+  console.log("Isso não é um dia válido");
+}
