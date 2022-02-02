4e5930e341798c6d5e6f5284de9b62d81db94a5e
Nádia <tristaonadia@gmail.com>
2022-02-02 15:27:17 -0300

    exercicios do dia 4-1: if e else.

---
+++ modulo1-fundamentos/bloco4-introducao-javascript-logica-programacao/dia4-1/C-if-else/script.js
@@ -0,0 +1,22 @@
+// let trybe = 16.20;
+
+// if (trybe >= 14 && trybe < 14.40) {
+//   console.log("Esquenta");
+// } else if (trybe >= 16.20 && trybe < 18) {
+//   console.log("Aula ao vivo");
+// } else if (trybe >= 19.40 && trybe < 20) {
+//   console.log("Fechamento");
+// } else {
+//   console.log("Fora dos momentos síncronos");
+// }
+
+let notaDesafioTecnico = 5435345123;
+console.log("Sua nota foi " + notaDesafioTecnico);
+
+if (notaDesafioTecnico >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (notaDesafioTecnico >= 60 && notaDesafioTecnico < 80) {
+  console.log("Você está na nossa lista de espera");
+} else {
+  console.log("Você foi reprovada(o)");
+}
