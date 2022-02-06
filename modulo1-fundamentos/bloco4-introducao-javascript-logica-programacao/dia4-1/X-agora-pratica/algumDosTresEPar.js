dcb4cf024f9d9fed9861dd9699d43aeabbcf4a97
Nádia <tristaonadia@gmail.com>
2022-02-06 13:03:28 -0300

    README.md do dia 4-1.

--- modulo1-fundamentos/bloco4-introducao-javascript-logica-programacao/dia4-1/X-agora-pratica/algumDosTresEPar.js
+++
@@ -1,9 +0,0 @@
-const primeiroNumero = 3;
-const segundoNumero = 5;
-const terceiroNumero = 9;
-
-if (primeiroNumero % 2 === 0 || segundoNumero % 2 === 0 || terceiroNumero % 2 === 0) {
-  console.log(true);
-} else {
-  console.log(false);
-}
