b363bfd3ff8a8df93300188bd70d0a3c7b04663b
Nádia <tristaonadia@gmail.com>
2022-02-14 22:12:07 -0300

    exercicios do dia 5-3: calendario tryber - exercício 5.

--- modulo1-fundamentos/bloco5-javascript-dom-eventos-web-storage/dia5-3/X-calendario-tryber/script.js
+++ modulo1-fundamentos/bloco5-javascript-dom-eventos-web-storage/dia5-3/X-calendario-tryber/script.js
@@ -103,3 +103,24 @@ btnHoliday.addEventListener('click', changeColorHolidays);
 */
 
 createButton('btn-friday', 'Sexta-feira');
+
+/*Exercício 5
+ Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+
+  - É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+*/
+
+function changeTextFridays() {
+  let fridayElements = document.querySelectorAll('.friday');
+  for (let i = 0; i < fridayElements.length; i += 1) {
+    let text = fridayElements[i].innerText;
+    if (text != 'Sextou!') {
+      fridayElements[i].innerText = 'Sextou!'
+    } else {
+      fridayElements[i].innerText = fridayList[i];
+    }
+  }
+}
+
+let btnFriday = document.querySelector('#btn-friday');
+btnFriday.addEventListener('click', changeTextFridays);
