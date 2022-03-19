24d4fcbbd35b7df2d9447e974009fbe082321427
nnnnadia <tristaonadia@gmail.com>
2022-03-18 22:37:40 -0300

    forEach por extenso

---
+++ modulo1-fundamentos/bloco8-higher-order-functions-javascript-es6/por-baixo-dos-panos/forEach.js
@@ -0,0 +1,5 @@
+const almoçoDeDomingo = ['🍌', '🥗', '🍲', '🍛', '🍮'];
+
+for(let index = 0; index < almoçoDeDomingo.length; index += 1) {
+  console.log(`${almoçoDeDomingo[index]} é o #${index} prato de ${almoçoDeDomingo}`);
+}
