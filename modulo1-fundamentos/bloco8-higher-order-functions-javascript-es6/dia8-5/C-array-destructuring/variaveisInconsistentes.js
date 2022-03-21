db98db20e0427dce2d715353ea1bf294c1c063af
nnnnadia <tristaonadia@gmail.com>
2022-03-21 20:13:02 -0300

    exercícios do dia 8-5: array destructuring - resolução exercício 2

--- modulo1-fundamentos/bloco8-higher-order-functions-javascript-es6/dia8-5/C-array-destructuring/variaveisInconsistentes.js
+++ modulo1-fundamentos/bloco8-higher-order-functions-javascript-es6/dia8-5/C-array-destructuring/variaveisInconsistentes.js
@@ -3,3 +3,6 @@ let animal = 'água';
 let bebida = 'arroz';
 
 console.log(comida, animal, bebida); // gato água arroz
+
+[comida, animal, bebida] = [bebida, comida, animal];
+console.log(comida, animal, bebida); // arroz gato água
