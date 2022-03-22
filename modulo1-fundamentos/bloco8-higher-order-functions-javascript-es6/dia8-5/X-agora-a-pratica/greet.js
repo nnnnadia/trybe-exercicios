df913c9c3167b2c7def093d313d7061b33a47e30
nnnnadia <tristaonadia@gmail.com>
2022-03-22 18:31:01 -0300

    exercício do dia 8-5: spread operator, parâmetro rest, destructuring e mais - resolução exercício 8

--- modulo1-fundamentos/bloco8-higher-order-functions-javascript-es6/dia8-5/X-agora-a-pratica/greet.js
+++ modulo1-fundamentos/bloco8-higher-order-functions-javascript-es6/dia8-5/X-agora-a-pratica/greet.js
@@ -1,4 +1,4 @@
-// escreva greet abaixo
+const greet = (name, greeting = 'Hi') => `${greeting} ${name}`;
 
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
