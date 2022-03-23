2cb69f6ade89e6dbeb986c033dd845b6f8f9a7f3
nnnnadia <tristaonadia@gmail.com>
2022-03-23 17:08:52 -0300

    exercício do dia 9-1: agora a prática - resolução exercício 7

--- modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-1/X-agora-a-pratica/uppercase.test.js
+++ modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-1/X-agora-a-pratica/uppercase.test.js
@@ -3,3 +3,15 @@ const uppercase = (str, callback) => {
     callback(str.toUpperCase());
   }, 500);
 };
+
+test('Testa a função uppercase()', (done) => {
+  const expected = 'EU NÃO TÔ ENTENDENDO NADA';
+  uppercase('eu não tô entendendo nada', (str) => {
+    try {
+      expect(str).toBe(expected);
+      done();
+    } catch {
+      done(error);
+    }
+  });
+});
