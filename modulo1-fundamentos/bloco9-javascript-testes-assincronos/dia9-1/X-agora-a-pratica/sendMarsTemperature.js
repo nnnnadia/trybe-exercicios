d68ec4f9c4d2b251452a435424d1e27d01a2ff51
nnnnadia <tristaonadia@gmail.com>
2022-03-23 16:53:26 -0300

    exercício do dia 9-1: agora a prática - template exercício 6

--- modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-1/X-agora-a-pratica/sendMarsTemperature.js
+++ modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-1/X-agora-a-pratica/sendMarsTemperature.js
@@ -3,7 +3,7 @@ const messageDelay = () => Math.floor(Math.random() * 5000);
 const getMarsTemperature = () => {
   const maxTemperature = 58;
   return Math.floor(Math.random() * maxTemperature);
-};
+}
 
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 
@@ -13,8 +13,14 @@ const temperatureInFahrenheit = (temperature) =>
 const greet = (temperature) =>
   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
 const sendMarsTemperature = (callback) => 
   setTimeout(() => callback(getMarsTemperature()), messageDelay());
 
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
