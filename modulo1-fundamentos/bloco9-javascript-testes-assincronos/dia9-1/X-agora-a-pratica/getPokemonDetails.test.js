811df77ed72399f14ccbc4f36962230f83407d52
nnnnadia <tristaonadia@gmail.com>
2022-03-23 17:43:20 -0300

    exercício do dia 9-1: agora a prática - resolução exercício 9

--- modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-1/X-agora-a-pratica/getPokemonDetails.test.js
+++ modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-1/X-agora-a-pratica/getPokemonDetails.test.js
@@ -1,12 +1,25 @@
-// Verifique se a importação do arquivo correto está sendo feita.
-const { getPokemonDetails } = require("./get-pokemon-details");
+const { getPokemonDetails } = require("./getPokemonDetails");
 
-describe("A função getPokemonDetails", () => {
+describe("A função getPokemonDetails", (done) => {
   it("retorna erro quando procuramos um pokemon que não existe no banco de dados", () => {
-    // Escreva aqui seu código
+    const expected = new Error('Não temos esse pokémon para você :(');
+
+    function callback(onError, onSuccess) {
+      expect(onError).toBe(expected);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'nãoéumpokemon', callback);
   });
 
   it("retorna um pokemon que existe no banco de dados", () => {
-    // Escreva aqui seu código
+    const expected = 'Olá, seu pokémon é o Bulbasaur, o tipo dele é Grass e a habilidade principal dele é Razor Leaf';
+
+    function callback(onError, onSuccess) {
+      expect(onSuccess).toBe(expected);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Bulbasaur', callback);
   });
 });
