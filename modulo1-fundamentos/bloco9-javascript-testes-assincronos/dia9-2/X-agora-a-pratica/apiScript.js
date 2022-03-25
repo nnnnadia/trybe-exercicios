92182588ae4155ee48113431c614992f8c29591d
nnnnadia <tristaonadia@gmail.com>
2022-03-25 16:09:12 -0300

    exercício do dia 9-2: agora a prática - resolução exercício 1

--- modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-2/X-agora-a-pratica/apiScript.js
+++ modulo1-fundamentos/bloco9-javascript-testes-assincronos/dia9-2/X-agora-a-pratica/apiScript.js
@@ -1,8 +1,19 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const showJoke = (joke) => {
+  const jokeContainer = document.querySelector('#jokeContainer');
+  jokeContainer.innerText = joke;
+}
+
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  }
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => showJoke(data.joke));
 };
 
 window.onload = () => fetchJoke();
