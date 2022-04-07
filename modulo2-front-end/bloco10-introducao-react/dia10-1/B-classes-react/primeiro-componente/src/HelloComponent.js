0a992504774c6c778dab5b8fb0517773ff93acf1
nnnnadia <tristaonadia@gmail.com>
2022-04-06 21:21:36 -0300

    10-1: exercícios - classes e react

---
+++ modulo2-front-end/bloco10-introducao-react/dia10-1/B-classes-react/primeiro-componente/src/HelloComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HelloComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Olá</h1>
+        <p>mundo</p>
+      </div>
+    );
+  }
+}
+
+export default HelloComponent;
