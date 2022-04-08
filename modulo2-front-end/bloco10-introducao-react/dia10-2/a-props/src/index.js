8e196bdc1f0493ead851ddf1bc0e0cc16e8e9fe3
nnnnadia <tristaonadia@gmail.com>
2022-04-08 14:43:38 -0300

    10-2: exercícios - props

---
+++ modulo2-front-end/bloco10-introducao-react/dia10-2/a-props/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Image from './Image';
+
+ReactDOM.render(
+  <Image src='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Cute cat staring' />,
+  document.getElementById('root')
+);
