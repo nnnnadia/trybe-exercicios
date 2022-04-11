730ddf31fb81c1202c0f0ef254084cefffe5b654
nnnnadia <tristaonadia@gmail.com>
2022-04-11 14:54:43 -0300

    11-1: exercícios - em resumo 1.res

--- modulo2-front-end/bloco11-componentes-estavo-eventos-formularios-react/dia11-1/a-introducao-aos-eventos/src/App.js
+++ modulo2-front-end/bloco11-componentes-estavo-eventos-formularios-react/dia11-1/a-introducao-aos-eventos/src/App.js
@@ -5,24 +5,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.isEven = this.isEven.bind(this);
     this.state = {
       firstCount: 0,
       secondCount: 0,
       thirdCount: 0,
     };
   }
-  handleClick = (which) => {
-    this.setState((prevState, _props) => ({
-      [which]: prevState[which] + 1,
-    }));
+  handleClick(which) {
+    this.setState((prevState, _props) => {
+      (prevState[which] % 2 === 0) ? console.log('blue') : console.log('green');
+      return {[which]: prevState[which] + 1};
+    });
+  }
+  isEven(which) {
+    return (this.state[which] % 2 === 0) ? 'even' : 'odd';
   }
   render() {
     const { firstCount, secondCount, thirdCount } = this.state;
     return (
       <>
-        <button onClick={() => this.handleClick('firstCount')}>{firstCount}</button>
-        <button onClick={() => this.handleClick('secondCount')}>{secondCount}</button>
-        <button onClick={() => this.handleClick('thirdCount')}>{thirdCount}</button>
+        <button className={this.isEven('firstCount')} onClick={() => this.handleClick('firstCount')}>{firstCount}</button>
+        <button className={this.isEven('secondCount')} onClick={() => this.handleClick('secondCount')}>{secondCount}</button>
+        <button className={this.isEven('thirdCount')} onClick={() => this.handleClick('thirdCount')}>{thirdCount}</button>
       </>
     );
   }
