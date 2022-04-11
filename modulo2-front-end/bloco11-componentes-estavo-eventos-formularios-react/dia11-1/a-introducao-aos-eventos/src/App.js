50fb239056d0b2a2d53285f8838d706d2ce54ace
nnnnadia <tristaonadia@gmail.com>
2022-04-11 12:33:45 -0300

    11-1: exercícios - unindo componentes com estados e eventos 1.res

--- modulo2-front-end/bloco11-componentes-estavo-eventos-formularios-react/dia11-1/a-introducao-aos-eventos/src/App.js
+++ modulo2-front-end/bloco11-componentes-estavo-eventos-formularios-react/dia11-1/a-introducao-aos-eventos/src/App.js
@@ -5,15 +5,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      firstCount: 0,
+      secondCount: 0,
+      thirdCount: 0,
+    };
   }
-  handleClick = (text) => console.log(text);
+  handleClick = (which) => {
+    this.setState((prevState, _props) => ({
+      [which]: prevState[which] + 1,
+    }));
+  }
   render() {
-    const { firstBtn, secondBtn, thirdBtn } = this.props;
+    const { firstCount, secondCount, thirdCount } = this.state;
     return (
       <>
-        <button onClick={() => this.handleClick(firstBtn)}>Red</button>
-        <button onClick={() => this.handleClick(secondBtn)}>Green</button>
-        <button onClick={() => this.handleClick(thirdBtn)}>Blue</button>
+        <button onClick={() => this.handleClick('firstCount')}>{firstCount}</button>
+        <button onClick={() => this.handleClick('secondCount')}>{secondCount}</button>
+        <button onClick={() => this.handleClick('thirdCount')}>{thirdCount}</button>
       </>
     );
   }
