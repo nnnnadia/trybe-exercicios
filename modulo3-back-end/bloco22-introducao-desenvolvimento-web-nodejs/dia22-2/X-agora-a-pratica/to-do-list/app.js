9a34ddd9856883a2339848da6a1cd9235e509f42
nnnnadia <tristaonadia@gmail.com>
2022-09-05 12:41:00 -0300

    22-2: agora a prática - exercício 4

--- modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-2/X-agora-a-pratica/to-do-list/app.js
+++ modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-2/X-agora-a-pratica/to-do-list/app.js
@@ -2,4 +2,28 @@ const express = require('express');
 
 const app = express();
 
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+app.get('/myActivities/:id', (req, res) => {
+  const { params } = req;
+  const requestedActivity = activities.find(activity => activity.id === +params.id);
+  res.status(200).json(requestedActivity);
+});
+
 module.exports = app;
