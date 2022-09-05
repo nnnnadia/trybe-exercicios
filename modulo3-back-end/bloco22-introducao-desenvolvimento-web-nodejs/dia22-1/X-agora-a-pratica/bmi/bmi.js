d535981f288f0e3156f6f16e6342ed9015bd49da
nnnnadia <tristaonadia@gmail.com>
2022-09-05 15:22:33 -0300

    22-1: agora a prática - exercício 5

--- modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-1/X-agora-a-pratica/bmi/bmi.js
+++ modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-1/X-agora-a-pratica/bmi/bmi.js
@@ -2,8 +2,24 @@ const readLineSync = require('readline-sync');
 
 const weight = readLineSync.questionFloat('What is your weight? ');
 const height = readLineSync.questionFloat('What is your height? ');
-const bmi = () => (weight / height ** 2).toFixed(1);
+const calculateBMI = () => (weight / height ** 2).toFixed(1);
+const zoneBMI = (bmi) => {
+  if (bmi < 18.5) return 'underweight';
+  if (bmi >= 18.5 && bmi < 25) return 'healthy';
+  if (bmi >= 25 && bmi < 30) return 'overweight';
+  if (bmi >= 30 && bmi < 40) return 'obese';
+  return 'extremely obese';
+};
 
-console.log(`Your BMI is ${bmi()}`);
+const calculatedBMI = calculateBMI();
+const bandBMI = zoneBMI(calculatedBMI);
 
-module.exports = bmi;
+console.log(`Your BMI is ${calculatedBMI}.`);
+console.log(`You are ${bandBMI}.`);
+
+module.exports = {
+  calculateBMI,
+  calculatedBMI,
+  zoneBMI,
+  bandBMI,
+};
