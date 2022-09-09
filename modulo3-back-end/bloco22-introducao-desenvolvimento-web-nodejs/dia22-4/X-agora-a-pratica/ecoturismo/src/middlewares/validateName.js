ace8a98f8839ebc000ae1aedd520ac528c3277b8
nnnnadia <tristaonadia@gmail.com>
2022-09-09 19:52:09 -0300

    22-4: agora a prática - exercício 4

---
+++ modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-4/X-agora-a-pratica/ecoturismo/src/middlewares/validateName.js
@@ -0,0 +1,8 @@
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ message: 'O campo nome é obrigatório' });
+  if (name.length < 4) return res.status(400).json({ message: 'O campo nome deve ter pelo menos 4 caracteres' });
+  next();
+};
+
+module.exports = validateName;
--- modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-4/X-agora-a-pratica/ecoturismo/src/routes/activitiesRouter.js
+++ modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-4/X-agora-a-pratica/ecoturismo/src/routes/activitiesRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { validateName } = require('../middlewares');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateName, (req, res) => {
   return res.status(201).json({ message: 'Atividade cadastrada com sucesso' });
 });
 
