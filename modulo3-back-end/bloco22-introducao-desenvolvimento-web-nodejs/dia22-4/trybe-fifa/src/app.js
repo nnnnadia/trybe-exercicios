0f58daa4be63c19b26c230f3a34d5122ee7658db
nnnnadia <tristaonadia@gmail.com>
2022-09-09 14:46:33 -0300

    22-4: middlewares - exercício 2

--- modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-4/trybe-fifa/src/app.js
+++ modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-4/trybe-fifa/src/app.js
@@ -46,24 +46,17 @@ app.post('/teams', validateTeam, (req, res) => {
 
 app.put('/teams/:id', validateTeam, (req, res) => {
   const id = Number(req.params.id);
-  const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    const updated = { id, ...req.body };
-    teams.splice(index, 1, updated);
-    res.status(201).json(updated);
-  } else {
-    res.sendStatus(400);
-  }
+  const team = res.locals.team;
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
 });
 
 app.delete('/teams/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-  }
+  const team = res.locals.team;
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
   res.sendStatus(200);
 });
 
