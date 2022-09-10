c22963e44823d173685846ad648136595126a7e1
nnnnadia <tristaonadia@gmail.com>
2022-09-09 21:43:10 -0300

    22-4: agora a prática - exercício 6

--- modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-4/X-agora-a-pratica/ecoturismo/src/routes/activitiesRouter.js
+++ modulo3-back-end/bloco22-introducao-desenvolvimento-web-nodejs/dia22-4/X-agora-a-pratica/ecoturismo/src/routes/activitiesRouter.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const { validateName, validatePrice } = require('../middlewares');
+const { validateName, validatePrice, validateDescription } = require('../middlewares');
 const router = express.Router();
 
-router.post(
-  '/',
-  validateName,
-  validatePrice,
-  (req, res) =>  res.status(201).json({ message: 'Atividade cadastrada com sucesso' }));
+router.post('/', validateName, validatePrice, validateDescription, (req, res) => res.status(201).json({ message: 'Atividade cadastrada com sucesso' }));
 
 module.exports = router;
