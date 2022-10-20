e0211575e42f55d82f8f820f270128e9b05f03df
nnnnadia <tristaonadia@gmail.com>
2022-10-20 15:45:31 -0300

    3.6.1: agora a prática - exercício 3

--- modulo3-back-end/secao6-nodejs-orm-autenticacao/dia6-1/agora-o-sequelize/controller/book.controller.js
+++ modulo3-back-end/secao6-nodejs-orm-autenticacao/dia6-1/agora-o-sequelize/controller/book.controller.js
@@ -5,11 +5,23 @@ const getAll = async (_req, res) => {
     const books = await BookService.getAll();
     return res.status(200).json(books);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
     return res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const id = req.params;
+    const book = await BookService.getById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(404).json({ message: 'Book not found' });
   }
 };
 
 module.exports = {
   getAll,
+  getById,
 };
--- modulo3-back-end/secao6-nodejs-orm-autenticacao/dia6-1/agora-o-sequelize/index.js
+++ modulo3-back-end/secao6-nodejs-orm-autenticacao/dia6-1/agora-o-sequelize/index.js
@@ -5,5 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.get('/books', BookController.getAll);
+app.get('/books/:id', BookController.getById);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
