576854f9298a6d00c82ce704f14e7ddd2eb09fe3
nnnnadia <tristaonadia@gmail.com>
2022-10-20 14:17:20 -0300

    3.6.1: agora a prática - exercício 2

--- modulo3-back-end/secao6-nodejs-orm-autenticacao/dia6-1/agora-o-sequelize/index.js
+++ modulo3-back-end/secao6-nodejs-orm-autenticacao/dia6-1/agora-o-sequelize/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { BookController } = require('./controller');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.get('/books', BookController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
