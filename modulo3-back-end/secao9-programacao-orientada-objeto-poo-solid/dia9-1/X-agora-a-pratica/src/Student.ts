4de15956df5f34aee07fd234764d6cb9407cdca4
nnnnadia <tristaonadia@gmail.com>
2022-11-09 21:05:11 -0300

    3.9.1: agora a prática - exercise 2

--- modulo3-back-end/secao9-programacao-orientada-objeto-poo-solid/dia9-1/X-agora-a-pratica/src/Student.ts
+++ modulo3-back-end/secao9-programacao-orientada-objeto-poo-solid/dia9-1/X-agora-a-pratica/src/Student.ts
@@ -4,10 +4,16 @@
     Escreva uma classe cujos objetos representarão pessoas estudantes matriculadas em uma disciplina.
     Cada objeto dessa classe deve conter os seguintes dados:
       matrícula, nome, 4 notas de prova, 2 notas de trabalho.
+
+  Exercício 2:
+    Agora vamos adicionar à nossa classe de pessoa estudante os comportamentos.
+    Para isso adicione dois métodos:
+      um que calcula a soma das notas da pessoa estudante
+      e outro que calcula a média das notas da pessoa estudante.
 */
 
 export default class Student {
-  private _name: string;
+  name: string;
   private _testScores: {
     firstScore: number,
     secondScore: number,
@@ -33,8 +39,35 @@ export default class Student {
       secondScore: number,
     },
   ) {
-    this._name = name;
+    this.name = name;
     this._testScores = testScores;
     this._essayScores = essayScores;
+  }
+
+  getScoreSum(): {
+    testScoreSum: number,
+    essayScoreSum: number,
+  } {
+    const testScoreSum = Object.values(this._testScores)
+      .reduce((acc, curr) => acc + curr);
+    const essayScoreSum = Object.values(this._essayScores)
+      .reduce((acc, curr) => acc + curr);
+    return {
+      testScoreSum,
+      essayScoreSum,
+    };
+  }
+
+  getScoreAverage(): {
+    testScoreAverage: number,
+    essayScoreAverage: number,
+  } {
+    const scoreSum = this.getScoreSum();
+    const testScoreAverage = scoreSum.testScoreSum / 4;
+    const essayScoreAverage = scoreSum.essayScoreSum / 2;
+    return {
+      testScoreAverage,
+      essayScoreAverage,
+    };
   }
 }
