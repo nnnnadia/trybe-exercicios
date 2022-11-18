203a7df0102b39453504e57d2531cff211cac2d2
nnnnadia <tristaonadia@gmail.com>
2022-11-18 17:08:15 -0300

    3.9.1: sintaxe geral - exercises 5, 6 and 7

--- modulo3-back-end/secao9-programacao-orientada-objeto-poo-solid/dia9-1/A-sintaxe-geral/Tv.ts
+++ modulo3-back-end/secao9-programacao-orientada-objeto-poo-solid/dia9-1/A-sintaxe-geral/Tv.ts
@@ -1,9 +1,9 @@
 class Tv {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
 
   constructor(
     brand: string,
@@ -11,18 +11,33 @@ class Tv {
     resolution: string,
     connections: string[],
   ) {
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
 
   turnOn = (): void => {
-    console.log(`Tv ${this.brand} ${this.size}" is on!
-  Resolution: ${this.resolution}
-  Available connections: ${this.connections}`);
+    console.log(`Tv ${this._brand} ${this._size}" is on!
+  Resolution: ${this._resolution}
+  Available connections: ${this._connections}`);
   };
+
+  get connectedTo() {
+    if (this._connectedTo) return this._connectedTo;
+    return 'The TV is not connected';
+  }
+
+  set connectedTo(connection: string) {
+    if (this._connections.includes(connection)) {
+      this._connectedTo = connection;
+    } else {
+      console.log('Unavailable connection port');
+    }
+  }
 }
 
 const tv = new Tv('Samsung', 40, '1080p', ['HDMI', 'VGA', 'AVI', 'Bluetooth']);
 tv.turnOn();
+tv.connectedTo = 'HDMI';
+console.log(tv.connectedTo);
